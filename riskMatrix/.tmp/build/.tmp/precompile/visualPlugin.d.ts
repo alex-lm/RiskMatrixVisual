044169c7a1c67f13abbf84b442cd8477cc8ff1fe
Alex CR <alex.lemarchant@controlrisks.com>
2025-12-04 14:07:13 +0000

    With package built

--- riskMatrix/.tmp/build/.tmp/precompile/visualPlugin.d.ts
+++ riskMatrix/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -1,4 +1,4 @@
 import powerbiVisualsApi from "powerbi-visuals-api";
 import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var riskMatrix7201B7777AFA42C59E8B4EAB078713F7_DEBUG: IVisualPlugin;
-export default riskMatrix7201B7777AFA42C59E8B4EAB078713F7_DEBUG;
+declare var riskMatrix7201B7777AFA42C59E8B4EAB078713F7: IVisualPlugin;
+export default riskMatrix7201B7777AFA42C59E8B4EAB078713F7;
